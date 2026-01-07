b51a1ed198a39870cec60490d98db106beafa072
alex344b <alex344b@edu.mercantec.dk>
2026-01-07 14:40:47 +0100

    hguhvhhj

---
+++ Arduino dashboard/Components/Pages/CatFactDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Arduino_dashboard.Components.Pages;
+
+public class CatFactDTO
+{
+        public DateOnly dateAdded { get; set; }
+        public string fact { get; set; }
+        public int likes { get; set; }
+        public int dislikes { get; set;}
+}
